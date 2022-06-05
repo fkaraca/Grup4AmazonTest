9d7ef7427dae8269ccfc4bc77c0e33a22457af87
muhammetkarakaya <karakayaatc@gmail.com>
2022-06-06 00:13:34 +0200

    mouse arama1

--- Group4AmazonTests/src/test/java/Tests/SearchBox.java
+++ Group4AmazonTests/src/test/java/Tests/SearchBox.java
@@ -1,4 +1,23 @@
 package Tests;
 
-public class SearchBox {
+import Utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class SearchBox extends TestBase {
+    @Test
+    public void SearchBox(){
+        driver.get("http://amazon.com");
+        WebElement dropDown=driver.findElement(By.id("searchDropdownBox"));
+        Select select=new Select(dropDown);
+        select.selectByVisibleText("Computers");
+        WebElement searchBox=driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("mouse");
+        searchBox.submit();
+        WebElement mouseResult=driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        System.out.println("mouseResult = " + mouseResult.getText());
+
+    }
 }
